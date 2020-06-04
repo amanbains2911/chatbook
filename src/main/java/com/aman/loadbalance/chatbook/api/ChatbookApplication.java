89289f86dbc34feccd053f213c8654a654b8843d
amanbains2911 <64704147+amanbains2911@users.noreply.github.com>
2020-06-04 16:43:03 -0400

    Initial commit

---
+++ src/main/java/com/aman/loadbalance/chatbook/api/ChatbookApplication.java
@@ -0,0 +1,27 @@
+package com.aman.loadbalance.chatbook.api;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/chatbook-api")
+public class ChatbookApplication {
+
+	@Value("${server.port}")
+	private String port;
+	
+	@GetMapping("/chat")
+	public String chatNow() {
+		return "application is up on port : "+port;
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aman/loadbalance/chatbook/api/ChatbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aman.loadbalance.chatbook.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
